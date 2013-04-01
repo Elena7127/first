b501ae9f5661c55fc9135c0f09acf0d90f37b6ec
grigoreva <grigoreva@pclinux223-2.wud.lan>
2013-04-01 13:47:09 +0400

    create and check pipe-func

--- test.c
+++ test.c
@@ -5,8 +5,13 @@ int main(int argc,char *argv[],char *envp[])
 {
 
 extern char **environ;
+ int fd[2];
+  if(pipe(fd)<0){
+    printf("!!!!!!");
+    return 1;
+ }
   int pid = fork();
-  
+  // char descriptor[2];
   if(pid < 0){
     printf("error");
   } else if(pid == 0){
@@ -14,10 +19,20 @@ extern char **environ;
     ofs = fopen("child1.txt","wt");
     fprintf(ofs, "%d %d\n", pid, getpid());
     execle("/usr/bin/env", "/usr/bin/env", 0, envp);
-    fclose(ofs);
+    write(fd[1], "hello", 6); 
+    close(fd[1]);
   }
   else{
+    char exp[6];
+    int i=0;
     printf("parent %d %d\n", pid, getpid());
+    while(1){
+     
+      if( read(fd[0], exp, i)!=0)
+    i++;
+      else break ;
+    }
+    close(fd[0]);
   }
   return 0;
 } 
